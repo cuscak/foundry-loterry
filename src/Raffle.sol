292bb43c32d5c57a585f08d83b10b0d1ab25d0c8
Andrej Cuscak <cuscak@gmail.com>
2023-06-10 13:48:07 +0200

    init

---
+++ src/Raffle.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+/**
+ * @title A sample Lottery contract
+ * @author Andrej Cuscak
+ * @notice This contract is for creating a sample raffle contract
+ */
+contract Raffle {
+
+}
